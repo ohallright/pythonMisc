0d2f303ab066fb3c3da625aaae6a2e04ba0546e6
Liz Valentine <liz.valentine1@gmail.com>
2021-02-25 18:25:16 -0600

    Nearpod_v1

---
+++ Nearpod/Modules/.ipynb_checkpoints/fnImage-checkpoint.py
@@ -0,0 +1,33 @@
+import pyautogui
+import time
+
+# search page button, then click
+def FindClick(picture, seconds=10):      
+    seconds = int(seconds)
+    
+    # print(seconds, 'seconds')
+    for i in range(seconds):
+        loc = pyautogui.locateOnScreen(picture)
+        # print(loc)
+        # print(i)
+        if (loc != None): break
+        time.sleep(.2)
+    if (loc != None):
+        pyautogui.click(loc)
+        output = 'Clicked'
+    if (loc == None):
+        output = 'NotFound'
+    return output
+        
+# search page button
+def FindPic(picture, seconds=10):
+    # print(seconds, 'seconds')
+    for i in range(seconds):
+        loc = pyautogui.locateOnScreen(picture)
+        if (loc != None): break
+        time.sleep(.2)    
+    if (loc != None):
+        output = 'Found'
+    if (loc == None):
+        output = 'NotFound'
+    return output    
---
+++ Nearpod/ReadMe.txt
@@ -0,0 +1,9 @@
+User setup
+
+use with chrome as browser
+
+will need to pip install
+    -pandas
+    -pyautogui
+    -pillow
+    -openpyxl
